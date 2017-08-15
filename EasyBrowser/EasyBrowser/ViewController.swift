c29e0de1857d443dd2dfd0b7f272df0a0d1c4575
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-14 21:38:22 -0400

    Created a WKWebView object and loaded a URL

--- EasyBrowser/EasyBrowser/ViewController.swift
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -6,13 +6,43 @@
 //  Copyright © 2017 JoseMelendez. All rights reserved.
 //
 
+//Frameworks
+import WebKit
 import UIKit
 
-class ViewController: UIViewController {
+//Conform to the WKNavigation protocol
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    //WKWebView object displays interactive web content
+    
+    var webView: WKWebView!
+    
 
+    override func loadView() {
+        
+        //Create a WKWebView Object
+        webView = WKWebView()
+        
+        //Assign the webView navigationDelegate to the current class (self)
+        webView.navigationDelegate = self
+        
+        view = webView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //The location of a resource on a remote server. Create the url
+        //URL has to be http"s" not http
+        let url = URL(string: "https://www.youtube.com/")!
+        
+        //load the URL Requeset
+        webView.load(URLRequest(url: url))
+        
+        //Allow back forward navigation
+        webView.allowsBackForwardNavigationGestures = true
+        
     }
 
 }
